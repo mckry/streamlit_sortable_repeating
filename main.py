b16a039646425b8c4afe0e1c863a3b2258698c57
Maximilian Kryschi <maxkryschi@gmail.com>
2022-05-18 17:22:07 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
-
 st.set_page_config(
-     page_title="Sortable Reapeating Panel"
+     page_title="Sortable Repeating Panel"
 )
 
 with open("styles/style.css") as file:
@@ -15,7 +14,7 @@ if 'events' not in st.session_state:
 else:
     last_list = st.session_state['events']
     st.session_state['events'] = st.multiselect("Events", ['Dinner Party', 'Birthday Party', 'Marriage',
-                                                 'PhD Party', 'Party Party'], default=st.session_state['events'])
+                                                'PhD Party', 'Party Party'], default=st.session_state['events'])
     if last_list != st.session_state['events']:
         st.experimental_rerun()
 
@@ -30,7 +29,7 @@ if len(st.session_state['events']) > 0:
         with col2:
             event_date = st.date_input('Date', key=item)
         with col3:
-            number_of_guests = st.number_input('Number of Guests', min_value=0, max_value=100,step=1, key=item)
+            number_of_guests = st.number_input('Number of Guests', min_value=0, max_value=100, step=1, key=item)
 
         single_event = {
                         'event': item,
@@ -47,4 +46,3 @@ if len(st.session_state['events']) > 0:
             interim_event_list_sorted_by_dates.append(item['event'])
         st.session_state['events'] = interim_event_list_sorted_by_dates
         st.experimental_rerun()
-
