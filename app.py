a5de5963b80753128fb087fac70890143aa33c46
Maximilian Kryschi <maxkryschi@gmail.com>
2022-06-24 18:50:53 +0200

    code formatted

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
 st.set_page_config(
-     page_title="Sortable Repeating Panel"
+    page_title="Sortable Repeating Panel"
 )
 
 with open("styles/style.css") as file:
     st.markdown(f'<style>{file.read()}</style>', unsafe_allow_html=True)
 
-events = ['Dinner Party', 'Birthday Party', 'Marriage', 'PhD Party', 'Party Party', 'Oktoberfest', 'Christmas Party', 'Carnival']
+events = ['Dinner Party', 'Birthday Party', 'Marriage', 'PhD Party', 'Party Party', 'Oktoberfest', 'Christmas Party',
+          'Carnival']
 # ----------------------------------------------------------------------------------------------------------------------
 st.markdown('### Sort events by Dates')
 
@@ -15,7 +16,8 @@ if 'events_sortable_by_date' not in st.session_state:
     st.session_state['events_sortable_by_date'] = st.multiselect("events sortable by date", events)
 else:
     last_list = st.session_state['events_sortable_by_date']
-    st.session_state['events_sortable_by_date'] = st.multiselect("events sortable by date", events, default=st.session_state['events_sortable_by_date'])
+    st.session_state['events_sortable_by_date'] = st.multiselect("events sortable by date", events,
+                                                                 default=st.session_state['events_sortable_by_date'])
     if last_list != st.session_state['events_sortable_by_date']:
         st.experimental_rerun()
 
@@ -32,10 +34,10 @@ if len(st.session_state['events_sortable_by_date']) > 0:
             number_of_guests = st.number_input('Number of Guests', min_value=0, max_value=100, step=1, key=item)
 
         single_event = {
-                        'event': item,
-                        'date': event_date,
-                        'number of guests': number_of_guests
-                        }
+            'event': item,
+            'date': event_date,
+            'number of guests': number_of_guests
+        }
         list_of_events_sorted_by_date.append(single_event)
 
     sort_events = st.button('Sort by Date')
@@ -54,7 +56,9 @@ if 'events_sortable_by_buttons' not in st.session_state:
     st.session_state['events_sortable_by_buttons'] = st.multiselect("events sortable by buttons", events)
 else:
     last_list = st.session_state['events_sortable_by_buttons']
-    st.session_state['events_sortable_by_buttons'] = st.multiselect("events sortable by buttons", events, default=st.session_state['events_sortable_by_buttons'])
+    st.session_state['events_sortable_by_buttons'] = st.multiselect("events sortable by buttons", events,
+                                                                    default=st.session_state[
+                                                                        'events_sortable_by_buttons'])
     if last_list != st.session_state['events_sortable_by_buttons']:
         st.experimental_rerun()
 
@@ -64,11 +68,13 @@ def sort_by_buttons(event_list: list):
     if len(event_list) > 0:
         for index_2, item_2 in enumerate(event_list):
             container_sortable_by_buttons = st.container()
-            col1_sortable_by_buttons, col2_sortable_by_buttons, col3_sortable_by_buttons, col4_sortable_by_buttons = container_sortable_by_buttons.columns(4)
+            col1_sortable_by_buttons, col2_sortable_by_buttons, col3_sortable_by_buttons, col4_sortable_by_buttons = container_sortable_by_buttons.columns(
+                4)
             with col1_sortable_by_buttons:
                 st.write(item_2)
             with col2_sortable_by_buttons:
-                number_of_guests_2 = st.number_input('Number of Guests', min_value=0, max_value=100, step=1, key=f'{item_2}_button')
+                number_of_guests_2 = st.number_input('Number of Guests', min_value=0, max_value=100, step=1,
+                                                     key=f'{item_2}_button')
             with col3_sortable_by_buttons:
                 if index_2 > 0:
                     move_up = st.button('up', key=f'{item_2}_button_up')
@@ -82,10 +88,10 @@ def sort_by_buttons(event_list: list):
                         index_2 += 1
 
             single_event_sortable_by_buttons = {
-                            'event': item_2,
-                            'order index': index_2,
-                            'number of guests': number_of_guests_2
-                            }
+                'event': item_2,
+                'order index': index_2,
+                'number of guests': number_of_guests_2
+            }
             list_of_events_sortable_by_buttons.append(single_event_sortable_by_buttons)
 
         # the order index of the list element below (press move_down) has to be changed outside the loop
